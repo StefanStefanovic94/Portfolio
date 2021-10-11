5bd987634dadb9e6760df6c1e3bc9eeb610a2939
stefanstefanovic94 <stefan.stefanovic@kortechs.io>
2021-10-11 15:20:15 +0200

    added components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import { Col } from 'react-bootstrap';
-import { Switch } from 'react-router';
+import { Route, Switch } from 'react-router';
 import './App.css';
 import Sidebar from './components/sidebar/sidebar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/home/home';
+import About from './components/about/about';
+import Projects from './components/projects/projects';
+import Contact from './components/contact/contact';
 
 function App() {
   return (
     <div className="App">
+      <Col xs="3" sm="3" md="2" lg="2">
+        <Sidebar />
+      </Col>
       <Switch>
-        <Col xs="3" sm="3" md="2" lg="2">
-          <Sidebar />
-        </Col>
+        <Route exact path="/"><Home /></Route>
+        <Route exact path="/about"><About /></Route>
+        <Route exact path="/projects"><Projects /></Route>
+        <Route exact path="/contact"><Contact /></Route>
       </Switch>
     </div>
   );
--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,14 +1,23 @@
 import React from "react"
 import "./sidebar.scss"
-import { FaFacebookF } from 'react-icons/fa';
+import { FaFacebookF, FaInstagram, FaSkype } from 'react-icons/fa';
+import { GrLinkedinOption } from 'react-icons/gr'
+import { AiFillGithub } from 'react-icons/ai'
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
     return (
         <div className="wrapDiv">
             <div><img src="https://avatars.githubusercontent.com/u/60787166?v=4" /></div>
             <div><p> Stefan Stefanovic</p></div>
-            <div className="icons"><FaFacebookF  /></div>
-        </div>
+            <div className="icons"><FaFacebookF /><GrLinkedinOption /> <FaInstagram /><FaSkype /> <AiFillGithub /></div>
+            <div>
+                <Link to="/"> <h3>Home</h3> </Link>
+                <Link to="/about"><h3>About</h3></Link>
+                <Link to="/projects"><h3>Projects</h3></Link>
+                <Link to="/contact"><h3>Contact</h3></Link>
+            </div>
+        </div >
     )
 }
 
